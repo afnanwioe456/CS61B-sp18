1f8a8304e75bd846c54ee0640152454ebf318cd5
afnanwioe456 <qym1635@gmail.com>
2024-06-12 15:42:38 +0800

    first commit of proj1a

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,123 @@
+public class ArrayDeque<T> {
+
+    private int size;
+    private T[] items;
+    private int nextFirst;
+    private int nextLast;
+
+    public ArrayDeque() {
+        size = 0;
+        items = (T[]) new Object[8];
+        nextFirst = items.length - 1;
+        nextLast = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return nextLast - nextFirst == 0
+                || nextFirst - nextLast == items.length - 1;
+    }
+
+    private void resizeLonger(boolean flag) {
+        T[] newItems = (T[]) new Object[2 * items.length];
+        int start = nextFirst;
+        if (flag) { start = movePointer(start, true); }
+        System.arraycopy(items, start, newItems, 0, items.length - start);
+        System.arraycopy(items, 0, newItems, items.length - start, start + size - items.length);
+        items = newItems;
+        nextFirst = items.length - 1;
+        nextLast = size;
+    }
+
+    private int movePointer(int pointer, boolean direction) {
+        if (direction) {
+            pointer++;
+            if (pointer == items.length) {
+                pointer = 0;
+            }
+        } else {
+            pointer --;
+            if (pointer < 0) {
+                pointer = items.length - 1;
+            }
+        }
+        return pointer;
+    }
+
+    public void addFirst(T item) {
+        items[nextFirst] = item;
+        size++;
+        if (nextFirst == nextLast) {
+            resizeLonger(false);
+        } else {
+            nextFirst = movePointer(nextFirst, false);
+        }
+    }
+
+    public void addLast(T item) {
+        items[nextLast] = item;
+        size++;
+        if (nextFirst == nextLast) {
+            resizeLonger(true);
+        } else {
+            nextLast = movePointer(nextLast, true);
+        }
+    }
+
+    private void resizeShorter() {
+        T[] newItems = (T[]) new Object[items.length / 2];
+        int start = movePointer(nextFirst, true);
+        System.arraycopy(items, start, newItems, 0, newItems.length - start);
+        if (start + size - items.length > 0)
+            System.arraycopy(items, 0, newItems, newItems.length - start, start + size - items.length);
+        items = newItems;
+        nextFirst = items.length - 1;
+        nextLast = size;
+    }
+
+    public void removeFirst() {
+        nextFirst = movePointer(nextFirst, true);
+        items[nextFirst] = null;
+        size--;
+        if (items.length * 0.25 >= size) {
+            resizeShorter();
+        }
+    }
+
+    public void removeLast() {
+        nextLast = movePointer(nextLast, false);
+        items[nextLast] = null;
+        size--;
+        if (items.length * 0.25 >= size) {
+            resizeShorter();
+        }
+    }
+
+    public void printDeque() {
+        for (int i = nextFirst + 1; i < nextFirst + size; i++) {
+            if (i < items.length) {
+                System.out.printf("%s ", items[i]);
+            } else {
+                System.out.printf("%s ", items[i - items.length]);
+            }
+        }
+        if (nextLast == 0) {
+            System.out.println(items[items.length - 1]);
+        } else {
+            System.out.println(items[nextLast - 1]);
+        }
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) { return null; }
+        int i = nextFirst + 1 + index;
+        if (i > items.length) {
+            i -= items.length;
+        }
+        return items[i];
+    }
+
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,105 @@
+public class LinkedListDeque<T> {
+
+    private static class ItemNode<T> {
+        T item;
+        ItemNode<T> prev;
+        ItemNode<T> next;
+
+        ItemNode(T item, ItemNode<T> prev, ItemNode<T> next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        ItemNode() {
+            this.item = null;
+            this.prev = this;
+            this.next = this;
+        }
+    }
+
+    private int size;
+    private final ItemNode<T> sentinel;
+
+    public LinkedListDeque() {
+        size = 0;
+        sentinel = new ItemNode<>();
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return sentinel.next == sentinel;
+    }
+
+    public void addFirst(T item) {
+        sentinel.next = new ItemNode<>(item, sentinel, sentinel.next);
+        sentinel.next.next.prev = sentinel.next;
+        size++;
+    }
+
+    public void addLast(T item) {
+        sentinel.prev = new ItemNode<>(item, sentinel.prev, sentinel);
+        sentinel.prev.prev.next = sentinel.prev;
+        size++;
+    }
+
+    public T removeFirst() {
+        ItemNode<T> firstNode = sentinel.next;
+        sentinel.next = firstNode.next;
+        sentinel.next.prev = sentinel;
+        size--;
+        return firstNode.item;
+    }
+
+    public T removeLast() {
+        ItemNode<T> lastNode = sentinel.prev;
+        sentinel.prev = lastNode.prev;
+        sentinel.prev.next = sentinel;
+        size--;
+        return lastNode.item;
+    }
+
+    public T get(int index) {
+        if (index >= size || index < 0) {
+            return null;
+        }
+        ItemNode<T> nodeP = sentinel.next;
+        int i = 0;
+        while (i < index) {
+            nodeP = nodeP.next;
+            i++;
+        }
+        return nodeP.item;
+    }
+
+    private T getRecursiveHelper(ItemNode<T> nodeP, int index) {
+        if (index < 0) {
+            return nodeP.item;
+        }
+        return getRecursiveHelper(nodeP.next, index - 1);
+    }
+
+    public T getRecursive(int index) {
+        if (index >= size || index < 0) {
+            return null;
+        }
+        return getRecursiveHelper(sentinel, index);
+    }
+
+    public void printDeque() {
+        if (size == 0) {
+            System.out.println();
+            return;
+        }
+        ItemNode<T> nodeP = sentinel;
+        while (nodeP.next.next != sentinel) {
+            nodeP = nodeP.next;
+            System.out.printf("%s ", nodeP.item);
+        }
+        System.out.println(nodeP.next.item);
+    }
+
+}
