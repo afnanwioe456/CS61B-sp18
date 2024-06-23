ba35a5fb08d215d0a8d20bb49a474b3a5af0dc34
afnanwioe456 <qym1635@gmail.com>
2024-06-23 15:44:44 +0800

    fixed style

--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -8,7 +8,7 @@ public class TestPalindrome {
 
     @Test
     public void testWordToDeque() {
-        Deque d = palindrome.wordToDeque("persiflage");
+        Deque<Character> d = palindrome.wordToDeque("persiflage");
         StringBuilder actual = new StringBuilder();
         for (int i = 0; i < "persiflage".length(); i++) {
             actual.append(d.removeFirst());
