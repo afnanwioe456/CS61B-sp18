03a9d1d545d4ea4d00e4738ae6d96c2bc56f6828
afnanwioe456 <qym1635@gmail.com>
2024-06-24 15:30:32 +0800

    added ArrayRingBuffer

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,79 @@
+package synthesizer;
+
+//TODO: Make sure to make this class and all of its methods public
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;
+    /* Index for the next enqueue. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private final T[] rb;
+
+    public int capacity() {
+        return capacity;
+    }
+
+    public int fillCount() {
+        return fillCount;
+    }
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        rb = (T[]) new Object[capacity];
+        first = 0;
+        last = 0;
+        fillCount = 0;
+        this.capacity = capacity;
+    }
+
+    private int movePointer(int p) {
+        if (p == capacity - 1) {
+            return 0;
+        }
+        return p + 1;
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    public void enqueue(T x) {
+        if (fillCount == capacity) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+        rb[last] = x;
+        last = movePointer(last);
+        fillCount++;
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    public T dequeue() {
+        if (fillCount == 0) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        T returnItem = rb[first];
+        first = movePointer(first);
+        fillCount--;
+        return returnItem;
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    public T peek() {
+        if (fillCount == 0) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        return rb[first];
+    }
+
+    // TODO: When you get to part 5, implement the needed code to support iteration.
+}
+
