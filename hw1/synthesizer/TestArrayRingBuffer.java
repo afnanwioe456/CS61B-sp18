5b200e2f2c39bfce8194d36a4c1ae704a3d826e9
afnanwioe456 <qym1635@gmail.com>
2024-06-24 16:14:31 +0800

    finished GuitarString

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -1,5 +1,7 @@
 package synthesizer;
+
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /** Tests the ArrayRingBuffer class.
@@ -9,11 +11,16 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Double> arb = new ArrayRingBuffer<>(10);
+        arb.enqueue(1.0);
+        arb.enqueue(2.0);
+        assertEquals(arb.fillCount, 2);
+        double item = arb.dequeue();
+        assertEquals(item, 1.0, 0.001);
     }
 
     /** Calls tests for ArrayRingBuffer. */
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
     }
-} 
+}
